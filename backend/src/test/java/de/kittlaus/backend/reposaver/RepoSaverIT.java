e3a38d2c8c330b5556f40d5d4c0d4459b42cbd69
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-06-25 00:15:17 +0200

    even more IT

--- backend/src/test/java/de/kittlaus/backend/reposaver/RepoSaverIT.java
+++ backend/src/test/java/de/kittlaus/backend/reposaver/RepoSaverIT.java
@@ -1,11 +1,14 @@
 package de.kittlaus.backend.reposaver;
 
+import de.kittlaus.backend.reposaver.models.GithubRepo;
 import de.kittlaus.backend.reposaver.models.NewSaverUser;
 import de.kittlaus.backend.reposaver.models.SaverUser;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -46,5 +49,23 @@ class RepoSaverIT {
 
         ResponseEntity<SaverUser> getResponse = testRestTemplate.getForEntity(URL +"find/"+ username, SaverUser.class);
         assertEquals(HttpStatus.NOT_FOUND,getResponse.getStatusCode());
+    }
+
+    @Test
+    void userCanAddRepoToListButNoDuplicates(){
+        String username = "jamarob";
+        ResponseEntity<SaverUser> postResponse = testRestTemplate.postForEntity(URL, new NewSaverUser(username), SaverUser.class);
+        GithubRepo repoToAdd = new GithubRepo("1","test","test");
+
+        ResponseEntity<SaverUser> putResponse = testRestTemplate.exchange(URL +"add/"+ username, HttpMethod.PUT,new HttpEntity<>(repoToAdd), SaverUser.class);
+        assertEquals(HttpStatus.OK,putResponse.getStatusCode());
+        SaverUser putBody = putResponse.getBody();
+        assertEquals(putBody.getSavedRepos().get(0),repoToAdd);
+
+        ResponseEntity<SaverUser> duplicateResponse = testRestTemplate.exchange(URL +"add/"+ username, HttpMethod.PUT,new HttpEntity<>(repoToAdd), SaverUser.class);
+        assertEquals(HttpStatus.CONFLICT,duplicateResponse.getStatusCode());
+
+        ResponseEntity<SaverUser> unknownResponse = testRestTemplate.exchange(URL +"add/johnDoe", HttpMethod.PUT,new HttpEntity<>(repoToAdd), SaverUser.class);
+        assertEquals(HttpStatus.NOT_FOUND,unknownResponse.getStatusCode());
     }
 }
